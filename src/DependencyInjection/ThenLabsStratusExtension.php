6ba2f913dc06d710a6a72cc83f953aeae5833820
Andy Daniel Navarro Taño <andaniel05@gmail.com>
2021-01-20 11:25:21 -0500

    Temp

---
+++ src/DependencyInjection/ThenLabsStratusExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ThenLabs\Bundle\StratusBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+/**
+ * @author Andy Daniel Navarro Taño <andaniel05@gmail.com>
+ */
+class ThenLabsStratusExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('services.yaml');
+    }
+}
