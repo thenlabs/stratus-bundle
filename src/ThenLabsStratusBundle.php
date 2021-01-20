0c98f9310d84eb8a6cc2ff9318903b1576b10863
Andy Daniel Navarro Taño <andaniel05@gmail.com>
2021-01-20 08:59:37 -0500

    Temp

---
+++ src/ThenLabsStratusBundle.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace ThenLabs\Bundle\StratusBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Andy Daniel Navarro Taño <andaniel05@gmail.com>
+ */
+class ThenLabsStratusBundle extends Bundle
+{
+}
